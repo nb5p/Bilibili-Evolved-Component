42084db0d9bec69da6673c8ea700a98ef87e7ba8
nb5p <2098464+nb5p@users.noreply.github.com>
2024-01-21 01:13:12 +0800

    Create remove-charge-button/index.js

---
+++ remove-charge-buttton/index.js
@@ -0,0 +1,23 @@
+exports.component = {
+  name: "removeChargeBtn",
+  displayName: "移除充电按钮",
+  description: {
+    "zh-CN": "移除关注按钮左侧的充电按钮",
+  },
+  entry: () => {},
+  instantStyles: [
+    {
+      name: "removeCharge",
+      style: ".new-charge-btn { display: none !important; }",
+    },
+  ],
+  tags: [
+    {
+      name: "style",
+      displayName: "样式",
+      color: "#8BC34A",
+      icon: "mdi-palette-outline",
+      order: 2,
+    },
+  ],
+};
